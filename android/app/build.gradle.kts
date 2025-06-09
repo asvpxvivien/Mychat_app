256cc2e7c620088c75b6ca9cb50dadd6c9871606
Vivien Amagbegnon <amagbegnonvivien7@gmail.com>
2025-06-09 23:55:20 +0100

    SDK new version fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.mychatapps.mychat"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = "27.0.12077973"
+        minSdk = 27
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
