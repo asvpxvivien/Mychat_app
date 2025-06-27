acaf4c954a7cff00f49cc86253ea1ea4031c3be3
Vivien Amagbegnon <amagbegnonvivien7@gmail.com>
2025-06-27 23:19:25 +0100

    Firestone database

---
+++ lib/main copy.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mychat/screens/splash_screen.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    final alreadyInitialized = Firebase.apps.any(
+      (app) => app.name == '[DEFAULT]',
+    );
+    if (!alreadyInitialized) {
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+    }
+  } on FirebaseException catch (e) {
+    if (e.code == 'duplicate-app') {
+      print("⚠️ Firebase app '[DEFAULT]' already initialized. Skipping.");
+    } else {
+      rethrow;
+    }
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+        fontFamily: "Poppins",
+      ),
+    );
+  }
+}
