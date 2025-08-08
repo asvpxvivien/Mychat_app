61742989049e9f3955137109dea491132f2fcbcf
Vivien Amagbegnon <amagbegnonvivien7@gmail.com>
2025-08-08 21:31:16 +0100

    Chatroom Screen

---
+++ lib/screens/chatroom_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatroomScreen extends StatefulWidget {
+  const ChatroomScreen({super.key});
+
+  @override
+  State<ChatroomScreen> createState() => _ChatroomScreenState();
+}
+
+class _ChatroomScreenState extends State<ChatroomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
