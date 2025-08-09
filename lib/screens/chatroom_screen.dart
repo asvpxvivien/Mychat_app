8161bc22db3368e569124bef05231e695b5c1d9c
Vivien Amagbegnon <amagbegnonvivien7@gmail.com>
2025-08-09 21:24:30 +0100

    Send Message Part

--- lib/screens/chatroom_screen.dart
+++ lib/screens/chatroom_screen.dart
@@ -22,13 +22,24 @@ class _ChatroomScreenState extends State<ChatroomScreen> {
 
       body: Column(
         children: [
-          Expanded(child: Container(color: Colors.red)),
+          Expanded(child: Container(color: Colors.white)),
 
           Container(
+            color: Colors.grey[300],
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
-                children: [Expanded(child: TextField()), Icon(Icons.send)],
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Type a message...",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Icon(Icons.send),
+                ],
               ),
             ),
           ),
